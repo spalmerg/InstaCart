0a0544823d794b9f92f18b0452a466880175d5ba
sarahpg <sarahpg@umich.edu>
2018-02-10 16:49:13 -0600

    working on the forms and website flow

--- App/views.py
+++ App/views.py
@@ -1,10 +1,26 @@
-from flask import render_template
+from flask import render_template, flash, request, redirect
 from App import app
+from wtforms import StringField, validators
+from App.forms import OrderForm, FlaskForm
+from model import
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('webpage.html',
-                           title='Home',
-                           user=user)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/homepage', methods=['GET', 'POST'])
+def homepage():
+  form = OrderForm(request.form)
+  
+  print(form.errors)
+  if request.method == 'POST':
+    item = request.form['item']
+
+    if form.validate():
+      recommendation = item
+      return render_template('/thankyou.html', item=item)
+    else:
+      flash("We don't have that item")
+  return render_template('homepage.html', form=form)
+
+@app.route('/thankyou')
+def thankyou():
+  return render_template('thankyou.html')
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+# sqlalchemy 
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# forms
+WTF_CSRF_ENABLED = True
+SECRET_KEY = os.environ.get("SECRET_KEY")
