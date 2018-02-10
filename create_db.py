e74f11cffb406aea3808f18820b59b91bb310b7a
sarahpg <sarahpg@umich.edu>
2018-02-10 11:42:08 -0600

    finished data loading

--- create_db.py
+++ create_db.py
@@ -1,47 +1,76 @@
-#from App import db
 import pandas as pd
-import sqlalchemy
 from sqlalchemy import create_engine, MetaData, Integer, Float, Table, Column, String
 from sqlalchemy import MetaData
 import config
 import os
 import psycopg2
+import io
 
+def format_surprise(orders,products,aisles):
+  data = pd.merge(orders, products, on = "product_id")
+  data = pd.merge(data, aisles, on = "aisle_id")
+  maxdf = pd.DataFrame(data.groupby(["order_id"])["add_to_cart_order"].agg("max"))
+  maxdf = maxdf.reset_index()
+  data = pd.merge(data, maxdf, on = "order_id")
+  data["rating"]=(1-data.add_to_cart_order_x/data.add_to_cart_order_y)*10
+  data = data[["order_id", "aisle_id", "rating"]].reset_index()
+  return(data)
+
+def top_items(orders,products):
+  df = pd.merge(orders, products, on = "product_id")[['order_id', 'product_id', 'aisle_id', 'product_name']]
+  df = df.groupby(['aisle_id', 'product_id']).agg('count')['order_id']
+  df = df.reset_index()
+  df = df.sort_values(['aisle_id', 'order_id'], ascending = False).groupby("aisle_id").first()
+  df = pd.merge(df, products, on = "product_id")[['aisle_id', 'product_name']]
+  return(df)
 
 class db_define(object):
   engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
   meta = MetaData(bind=engine)
 
-  orders = Table('orders', meta,
+  surprise = Table('surprise', meta,
+    Column('index', Float, primary_key=True, autoincrement=False),
     Column('order_id', Integer, primary_key=True, autoincrement=False),
-    Column('product_id', Integer, nullable=True),
-    Column('add_to_cart_order', Integer, nullable=True),
-    Column('reordered', String, nullable=True)
+    Column('aisle_id', Integer, primary_key=True, autoincrement=False),
+    Column('rating', Float, nullable=True),
   )
 
-  products = Table('products', meta,
-    Column('product_id', Integer, primary_key=True, autoincrement=False),
-    Column('product_name', String, nullable=True),
-    Column('aisle_id', Integer, nullable=True),
-    Column('department_id', Integer, nullable=True)
-    )
- 
-  aisle = Table('aisle', meta,
-    Column('aisle_id', Integer, primary_key=True, autoincrement=False),
-    Column('aisle', String, nullable=True),
-    )
+  popitems = Table('popitems', meta,
+    Column('aisle_id', Integer, primary_key=False, autoincrement=False),
+    Column('product_name', String, nullable=True)
+  )
+  meta.create_all()
 
 
 if __name__ == "__main__":
+  #set up database and connection
   db = db_define()
-  db.meta.create_all(db.engine)
+  connection = psycopg2.connect(
+    dbname = os.getenv("DATABASE"),
+    user = os.getenv("USERNAME"),
+    password = os.getenv("PASSWORD"),
+    host = os.getenv("HOST")
+    )
+  cur = connection.cursor()
 
-  con = create_engine(os.environ.get("DATABASE_URL"))
+#read in csv
+  orders = pd.read_csv("Data/order_products__train.csv")
+  products = pd.read_csv("Data/products.csv")
+  aisles = pd.read_csv("Data/aisles.csv")
 
-  orders = pd.read_csv("../instacart_2017_05_01/order_products__train.csv")
-  products = pd.read_csv("../instacart_2017_05_01/products.csv")
-  aisles = pd.read_csv("../instacart_2017_05_01/aisles.csv")
+#reformat data
+  surprise = format_surprise(orders,products,aisles)
+  popitems = top_items(orders,products)
 
-  orders.head().to_sql("orders", con, if_exists='replace', index=False)
-  products.head().to_sql("products", con, if_exists='replace', index=False)
-  aisles.head().to_sql("aisles", con, if_exists='replace', index=False)
+  buf_pop = io.StringIO()
+  buf_sup = io.StringIO()
+  surprise = surprise.to_csv(buf_sup, header=False, index=False, sep='\t')
+  popitems = popitems.to_csv(buf_pop, header=False, index=False, sep='\t')
+  buf_pop.seek(0)
+  buf_sup.seek(0)
+
+#write to database
+  cur.copy_from(buf_pop, 'popitems',columns=('aisle_id','product_name'))
+  cur.copy_from(buf_sup, 'surprise',columns=('index','order_id','aisle_id','rating'))
+  connection.commit()
+  connection.close()
