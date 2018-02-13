d54449d95a38518df59b413f9431843ea01967a2
sarahpg <sarahpg@umich.edu>
2018-02-13 14:24:02 -0600

    update requirements.txt

--- Docs/conf.py
+++ Docs/conf.py
@@ -38,6 +38,8 @@ extensions = ['sphinx.ext.autodoc']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+autodoc_mock_imports = ['sqlalchemy', '']
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
