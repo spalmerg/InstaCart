2cff52a2e2d92aacd8eb6d6b031a07216b70f75c
sarahpg <sarahpg@umich.edu>
2018-02-21 18:38:20 -0600

    update html/css

--- App/forms.py
+++ App/forms.py
@@ -8,5 +8,5 @@ from wtforms import SelectField, SubmitField
 choices = pickle.load(open("Develop/Model/rid_to_name.pkl", "rb")).items()
 
 class OrderForm(FlaskForm):
- item = SelectField(label='Item', choices=choices)
+ item = SelectField(label='What are you craving?', choices=choices)
  submit = SubmitField('Add to cart')
--- App/views.py
+++ App/views.py
@@ -8,14 +8,13 @@ import pickle
 model = pickle.load(open("Develop/Model/model.pkl", "rb"))
 choices = pickle.load(open("Develop/Model/rid_to_name.pkl", "rb"))
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 @app.route('/homepage', methods=['GET', 'POST'])
 def homepage():
-  form = OrderForm(request.form)
+  form=OrderForm(request.form)
 
   if request.method == 'POST':
-    item = request.form['item']
-
+    item=request.form['item']
     if form.validate():
       neighbors=give_recommendation(model,item,choices)
       return render_template('/thankyou.html', item=neighbors)
